f74d836b403b4fcef7351e598b792c3e79b3b0cc
*Gustavo <guga77sugu@hotmail.com>
2023-08-03 17:36:40 -0300

    Concatenation with ".format()"

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 print('Hello World!')
 
+# nome=input('Qual é o seu nome?')
+# sobrenome=input('Qual é o seu sobrenome?')
+# message='Seja bem vindo, '
+# print(message, nome, sobrenome + '!')
 
-nome=input('Qual é o seu nome?')
-sobrenome=input('Qual é o seu sobrenome?')
-message='Seja bem vindo, '
-print(message, nome, sobrenome + '!')
+n1 =int(input('Digite um número: '))
+n2 =int(input('Digite outro número: '))
+soma = n1 + n2
+print('A soma de {} e {} resulta em {}'.format(n1, n2, soma))
