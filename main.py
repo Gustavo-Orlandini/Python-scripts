3f391969e8c07ccde803fe9278d4921fae967205
*Gustavo <guga77sugu@hotmail.com>
2023-08-04 17:00:33 -0300

    If and elif

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-print('Hello World!')
+print('\033[1;34;45mHello World!\033[0m')
 
 # nome=input('Qual é o seu nome?')
 # sobrenome=input('Qual é o seu sobrenome?')
 # message='Seja bem vindo, '
 # print(message, nome, sobrenome + '!')
 
-n1 =int(input('Digite um número: '))
-n2 =int(input('Digite outro número: '))
-soma = n1 + n2
-print('A soma de {} e {} resulta em {}'.format(n1, n2, soma))
+# n1 =int(input('Digite um número: '))
+# n2 =int(input('Digite outro número: '))
+# soma = n1 + n2
+# print('A soma de {} e {} resulta em {}'.format(n1, n2, soma))
+
+nome = str(input('Qual é o seu nome? '))
+if nome == 'Gustavo':
+    print('Que nome bonito!')
+elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
+    print('Seu nome é muito popular no Brasil.')
+elif nome in 'Ana Cláudia Jéssica Juliana':
+    print('Belo nome feminino!')    
+print('Tenha um bom dia, {}!'.format(nome))
+
